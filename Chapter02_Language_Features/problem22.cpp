5947c2ca28c39ffd867f0a9cad371b4b95c2490f
john <johndpowell02@gmail.com>
2022-01-12 21:08:49 -0600

    add Scale enum & mPreferredScale overload =, ==, <, >, <=, >=, <<, >> get temp value from preferred scale or given scale

--- Chapter02_Language_Features/problem22.cpp
+++ Chapter02_Language_Features/problem22.cpp
@@ -5,21 +5,49 @@
  */
 
 #include <iostream>
+#include <sstream>
 
 struct Temperature {
+public:
+    enum Scale {
+        Celsius,
+        Fahrenheit,
+        Kelvin
+    };
+
 private:
     friend Temperature operator"" _deg(long double val);
     friend Temperature operator"" _f(long double val);
     friend Temperature operator"" _K(long double val);
 
-    long double mCelsius{0};
-public:
-    explicit Temperature(long double val)
-        : mCelsius{val} {}
+    long double mCelsius;
+    Scale mPreferredScale;
 
-    long double getCelsius() const    { return mCelsius; }
-    long double getFahrenheit() const { return (9.f/5.f * mCelsius + 32); }
-    long double getKelvin() const     { return (mCelsius + 273.15); }
+    long double getCelsius() const     { return mCelsius; }
+    long double getFahrenheit() const  { return (9.f/5.f * mCelsius + 32); }
+    long double getKelvin() const      { return (mCelsius + 273.15); }
+
+public:
+    Temperature()
+        : mCelsius{0}, mPreferredScale{Scale::Celsius} {}
+    explicit Temperature(long double val)
+        : mCelsius{val}, mPreferredScale{Scale::Celsius} {}
+    Temperature(long double val, Scale scale)
+        : mCelsius{val}, mPreferredScale{scale} {}
+
+    // Returns the value of the temperature in the preferred scale
+    long double get() const;
+    // Returns the value of the temperature in the given scale
+    long double get(Scale scale) const;
+
+    Scale getPreferredScale() const      { return mPreferredScale; }
+    void setPreferredScale(Scale scale)  { mPreferredScale = scale; }
+
+    Temperature& operator=(const Temperature& t) {
+        mCelsius = t.getCelsius();
+        mPreferredScale = t.getPreferredScale();
+        return *this;
+    }
 
     Temperature operator+(Temperature other) {
         return Temperature(getCelsius() + other.getCelsius());
@@ -36,26 +64,69 @@ public:
     Temperature operator/(long double divisor) {
         return Temperature(getCelsius() / divisor);
     }
+
+    friend bool operator==(const Temperature& lhs, const Temperature& rhs) {
+        return lhs.getCelsius() == rhs.getCelsius();
+    }
+
+    friend bool operator<(const Temperature& lhs, const Temperature& rhs) {
+        return lhs.getCelsius() < rhs.getCelsius();
+    }
+
+    friend bool operator>(const Temperature& lhs, const Temperature& rhs) {
+        return lhs.getCelsius() > rhs.getCelsius();
+    }
+
+    friend bool operator<=(const Temperature& lhs, const Temperature& rhs) {
+        return lhs.getCelsius() <= rhs.getCelsius();
+    }
+
+    friend bool operator>=(const Temperature& lhs, const Temperature& rhs) {
+        return lhs.getCelsius() >= rhs.getCelsius();
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Temperature& t) {
+        os << t.get();
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, Temperature& t) {
+        long double val;
+        is >> val;
+        // The preferred temperature scale will default to Celsius
+        t = Temperature(val);
+        return is;
+    }
 };
 
 Temperature operator"" _deg(long double val) {
-    return Temperature(val);
+    return Temperature(val, Temperature::Scale::Celsius);
 }
 Temperature operator"" _f(long double val) {
-    return Temperature(5.f/9.f * (val - 32));
+    return Temperature((5.f/9.f * (val - 32)), Temperature::Scale::Fahrenheit);
 }
 Temperature operator"" _K(long double val) {
-    return Temperature(val - 273.15);
+    return Temperature(val - 273.15, Temperature::Scale::Kelvin);
+}
+
+long double Temperature::get() const {
+    return get(mPreferredScale);
+}
+
+long double Temperature::get(Scale scale) const {
+    switch (scale) {
+        case Temperature::Scale::Celsius:     return getCelsius();
+        case Temperature::Scale::Fahrenheit:  return getFahrenheit();
+        case Temperature::Scale::Kelvin:      return getKelvin();
+    }
 }
 
 
 int main() {
-    Temperature temp{100.0_deg};
-    Temperature temp2{50.0_deg};
+    Temperature temp{100.0_f};
+    Temperature temp1;
+    temp1 = temp;
+    Temperature::Scale scale{temp1.getPreferredScale()};
 
-    Temperature t1 = temp / 2;
-    std::cout << "Celsius: " << t1.getCelsius()
-              << "\nFahrenheit: " << t1.getFahrenheit()
-              << "\nKelvin: " << t1.getKelvin()
-              << std::endl;
+    std::cout << scale << std::endl;
 }
